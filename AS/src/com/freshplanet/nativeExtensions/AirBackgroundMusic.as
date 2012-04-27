967d4e6f9b69a95ff360178324520bed0383806a
Thibaut Crenn <crenn@freshplanet.com>
2012-04-27 14:39:03 -0400

    Adds ant build

--- AS/src/com/freshplanet/nativeExtensions/AirBackgroundMusic.as
+++ AS/src/com/freshplanet/nativeExtensions/AirBackgroundMusic.as
@@ -1,3 +1,21 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright 2012 Freshplanet (http://freshplanet.com | opensource@freshplanet.com)
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  
+//////////////////////////////////////////////////////////////////////////////////////
+
 package com.freshplanet.nativeExtensions
 {
 	import flash.external.ExtensionContext;
@@ -22,7 +40,10 @@ package com.freshplanet.nativeExtensions
 			return _instance != null ? _instance : new AirBackgroundMusic()
 		} 
 		
-
+		/**
+		 * Init the audio session.
+		 * Disable all background sounds that were playing before.
+		 */
 		public function init():void
 		{
 			if (this.useNativeExtension())
@@ -31,6 +52,10 @@ package com.freshplanet.nativeExtensions
 			}
 		}
 		
+		/**
+		 * Check if the current device can use the native extension.
+		 * For now, only iOS devices can do it.
+		 */
 		private function useNativeExtension():Boolean
 		{
 			return Capabilities.manufacturer.indexOf("iOS") > -1;
--- NativeIOS/AirBackgroundMusic/AirBackgroundMusic.m
+++ NativeIOS/AirBackgroundMusic/AirBackgroundMusic.m
@@ -1,10 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////////////
 //
-//  AirBackgroundMusic.m
-//  AirBackgroundMusic
-//
-//  Created by Thibaut Crenn on 4/20/12.
-//  Copyright 2012 __MyCompanyName__. All rights reserved.
-//
+//  Copyright 2012 Freshplanet (http://freshplanet.com | opensource@freshplanet.com)
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  
+//////////////////////////////////////////////////////////////////////////////////////
 
 #import "AirBackgroundMusic.h"
 #define DEFINE_ANE_FUNCTION(fn) FREObject (fn)(FREContext context, void* functionData, uint32_t argc, FREObject argv[])
@@ -24,9 +34,15 @@
 
 @end
 
+// initAudio()
+// Set and active the new session audio.
+// The session audio used is AVAudioSessionCategoryPlayback
+// @see http://developer.apple.com/library/ios/#documentation/Audio/Conceptual/AudioSessionProgrammingGuide/Configuration/Configuration.html#//apple_ref/doc/uid/TP40007875-CH3-SW1
+// returns the newly activated session name
 DEFINE_ANE_FUNCTION(initAudio)
 {
     // silence everything.
+    [[AVAudioSession sharedInstance] setActive: NO error: nil];
     [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:NULL];
     [[AVAudioSession sharedInstance] setActive: YES error: nil];
     
@@ -36,11 +52,11 @@ DEFINE_ANE_FUNCTION(initAudio)
     
     // Prepare for AS3
     FREObject retStr;
-	FRENewObjectFromUTF8(strlen(str)+1, (const uint8_t*)str, &retStr);
+    FRENewObjectFromUTF8(strlen(str)+1, (const uint8_t*)str, &retStr);
     return retStr;
 }
 
-// ContextInitializer()
+// AirBgMusicContextInitializer()
 //
 // The context initializer is called when the runtime creates the extension context instance.
 void AirBgMusicContextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx, 
@@ -58,19 +74,14 @@ void AirBgMusicContextInitializer(void* extData, const uint8_t* ctxType, FRECont
     *functionsToSet = func;    
 }
 
-// ContextFinalizer()
+// AirBgMusicContextFinalizer()
 //
 // Set when the context extension is created.
-
-void AirBgMusicContextFinalizer(FREContext ctx) { 
-    NSLog(@"Entering ContextFinalizer()");
-    
-    NSLog(@"Exiting ContextFinalizer()");	
-}
+void AirBgMusicContextFinalizer(FREContext ctx) {}
 
 
 
-// AirFlurryInitializer()
+// AirBgMusicInitializer()
 //
 // The extension initializer is called the first time the ActionScript side of the extension
 // calls ExtensionContext.createExtensionContext() for any context.
@@ -78,11 +89,8 @@ void AirBgMusicContextFinalizer(FREContext ctx) {
 void AirBgMusicInitializer(void** extDataToSet, FREContextInitializer* ctxInitializerToSet, FREContextFinalizer* ctxFinalizerToSet ) 
 {
     
-    NSLog(@"Entering ExtInitializer()");                    
+    *extDataToSet = NULL;
+    *ctxInitializerToSet = &AirBgMusicContextInitializer; 
+    *ctxFinalizerToSet = &AirBgMusicContextFinalizer;
     
-	*extDataToSet = NULL;
-	*ctxInitializerToSet = &AirBgMusicContextInitializer; 
-	*ctxFinalizerToSet = &AirBgMusicContextFinalizer;
-    
-    NSLog(@"Exiting ExtInitializer()"); 
 }
